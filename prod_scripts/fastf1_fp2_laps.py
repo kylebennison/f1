504ac659bf9ea6480dffe23feeecc308af2ba301
kylebennison <kylebeni012@gmail.com>
2022-03-24 20:36:12 -0400

    add round and year variables

--- prod_scripts/fastf1_fp2_laps.py
+++ prod_scripts/fastf1_fp2_laps.py
@@ -14,8 +14,9 @@ import os
 # import seaborn as sns
 import pandas as pd
 
-round_num = 1
+round_num = 22
 session_name = 'FP2'
+year = 2021
 
 start = r'C:\Users\Kyle\Documents\Projects\Data Projects\f1'
 
@@ -23,12 +24,12 @@ direct = start+'\\fastf1_cache'
 
 ff1.Cache.enable_cache(direct)
 
-event  = ff1.get_event(2022, round_num)
+event  = ff1.get_event(year, round_num)
 
-session = ff1.get_session(2022, round_num, session_name)
+session = ff1.get_session(year, round_num, session_name)
 
 session_data = session.load(laps = True, telemetry = True, weather = False)
 
 laps = session.load_laps()
 
-laps.to_csv(start + "\\timing_data\\2022_" + str(round_num) + "_" + session_name + "_laps.csv", index = False)
+laps.to_csv(start + "\\timing_data\\" + str(year) + "_" + str(round_num) + "_" + session_name + "_laps.csv", index = False)
