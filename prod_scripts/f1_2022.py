f7543bc65b5085f8e48aa95f8ca0496667b6c937
kylebennison <kylebeni012@gmail.com>
2022-03-27 23:33:57 -0400

    add variables, try except

--- prod_scripts/f1_2022.py
+++ prod_scripts/f1_2022.py
@@ -14,36 +14,48 @@ import os
 # import seaborn as sns
 import pandas as pd
 
+year = int(input(prompt="Year: "))
+round_num = int(input(prompt="Round: "))
+session_name = input("Session: ")
+
 start = r'C:\Users\Kyle\Documents\Projects\Data Projects\f1'
 
 direct = start+'\\fastf1_cache'
 
 ff1.Cache.enable_cache(direct)
 
-event  = ff1.get_event(2022, 1)
+event = ff1.get_event(year, round_num)
 
-session = ff1.get_session(2022, 1, 'Q')
+session = ff1.get_session(year, round_num, session_name)
 
-session_data = session.load(laps = True, telemetry = True, weather = False)
+session_data = session.load(laps=True, telemetry=True, weather=False)
 
 laps = session.load_laps()
 
-laps.to_csv(start+"\\timing_data\\2022_1_q_laps.csv", index = False)
+laps.to_csv(start+"\\timing_data\\"+str(year)+"_"+str(round_num) +
+            "_"+session_name+"_laps.csv", index=False)
 
 telem_final = pd.DataFrame()
 
 # Get fastest lap for each driver and get telemetry from lap
 for driver in laps['Driver'].unique():
-    telem = laps.pick_driver(driver).pick_fastest().get_car_data().add_distance()
+    try:
+        telem = laps.pick_driver(
+            driver).pick_fastest().get_car_data().add_distance()
 
-    # Add column with driver name
-    telem['Driver'] = driver
-    
-    team = laps.pick_driver(driver).pick_fastest()['Team']
-    
-    telem['Team'] = team
-    
-    # combine to master table
-    telem_final = pd.concat([telem, telem_final])
+    except:
+        continue
 
-telem_final.to_csv(start+"\\timing_data\\2022_1_q_telem_fastest.csv", index = False)
+    else:
+        # Add column with driver name
+        telem['Driver'] = driver
+
+        team = laps.pick_driver(driver).pick_fastest()['Team']
+
+        telem['Team'] = team
+
+        # combine to master table
+        telem_final = pd.concat([telem, telem_final])
+
+telem_final.to_csv(start+"\\timing_data\\"+str(year)+"_" +
+                   str(round_num)+"_"+session_name+"_telem_fastest.csv", index=False)
