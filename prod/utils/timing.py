c4d55c6aa6c8e6b5ab3aa39fc1287db327af0045
kylebennison <kylebeni012@gmail.com>
2023-03-04 22:07:08 -0500

    telem plots, pick_fastest telem option

--- prod/utils/timing.py
+++ prod/utils/timing.py
@@ -27,17 +27,33 @@ def get_team_names(session: f1.core.Session):
     return driver_team_map
 
 
-def get_telem(session: f1.core.Session):
+def get_telem(
+    session: f1.core.Session, fastest_only: bool = False, drivers: list = None
+):
     df = pd.DataFrame()
 
-    for f in session.car_data.keys():
-        new = session.car_data[f]
-        new["Car"] = f
-        df = pd.concat([df, new], axis=0)
+    # Get only the fastest lap per driver
+    if fastest_only:
+        for car in session.laps.DriverNumber.unique():
+            lap = (
+                session.laps.loc[session.laps["DriverNumber"] == car, :]
+                .pick_fastest()
+                .get_car_data()
+                .add_distance()
+            )
+            lap["DriverNumber"] = car
+            df = pd.concat([df, lap], axis=0)
+
+    # Get all telem
+    else:
+        for f in session.car_data.keys():
+            new = session.car_data[f]
+            new["DriverNumber"] = f
+            df = pd.concat([df, new], axis=0)
 
     # Add driver names
     mapping = get_driver_numbers(session)
-    df["Driver"] = df["Car"].map(mapping)
+    df["Driver"] = df["DriverNumber"].map(mapping)
 
     # Add team names
     mapping = get_team_names(session)
